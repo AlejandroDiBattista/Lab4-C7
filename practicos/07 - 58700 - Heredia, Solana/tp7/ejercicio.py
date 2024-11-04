08fcefec798eceb598ea128c7e4e8047ad197ce1
solheredia <144463383+solheredia@users.noreply.github.com>
2024-11-04 18:35:49 -0300

    avancesRedNeuronal

--- practicos/07 - 58700 - Heredia, Solana/tp7/ejercicio.py
+++ practicos/07 - 58700 - Heredia, Solana/tp7/ejercicio.py
@@ -13,3 +13,27 @@ import matplotlib.pyplot as plt
 ## Guardar Modelo
 ## Graficar Predicciones
 st.title('Estimación de Ventas Diarias')
+@st.cache
+def cargar_datos():
+    datos = pd.read_csv("ventas.csv")
+    return datos
+datos = cargar_datos()
+st.write("datos de ventas", datos)
+def normalizar_datos(datos):
+    dato['ventas_normalizadas'] = (dato['ventas'] - dato['ventas'].min()) / (dato['ventas'].max() - dato['ventas'].min())
+    return datos
+dato = normalizar_datos(datos)
+st.write('Datos normalizados', dato)
+
+class ventasNN(nn.Module):
+    def __init__(self):
+        super(ventasNN,self).__init__()
+        self.fc1 = nn.Linear(1,10)
+        self.fc2 = nn.Linear(10,5)
+        self.fc3 = nn.Linear(5,1)
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+modelo = ventasNN()      
