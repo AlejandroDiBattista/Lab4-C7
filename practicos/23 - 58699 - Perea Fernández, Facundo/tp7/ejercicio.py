8dd7ebc4bf56a697d3b778346004fb0fe7b4371d
FFFacundo <utn.facundopff@gmail.com>
2024-11-07 12:20:21 -0300

    FRONT

--- practicos/23 - 58699 - Perea Fernández, Facundo/tp7/ejercicio.py
+++ practicos/23 - 58699 - Perea Fernández, Facundo/tp7/ejercicio.py
@@ -13,3 +13,19 @@ import matplotlib.pyplot as plt
 ## Guardar Modelo
 ## Graficar Predicciones
 st.title('Estimación de Ventas Diarias')
+
+st.sidebar.header("Parámetros de la Red Neuronal")
+
+
+learning_rate = st.sidebar.slider("Tasa de aprendizaje", min_value=0.0, max_value=1.0, value=0.1, step=0.01)
+
+epochs = st.sidebar.slider("Cantidad de épocas", min_value=10, max_value=10000, value=100, step=10)
+
+hidden_neurons = st.sidebar.slider("Cantidad de neuronas en la capa oculta", min_value=1, max_value=100, value=5, step=1)
+
+
+if st.sidebar.button("Entrenar"):
+    # La red neuronal iria aqui
+    pass
+
+st.header("Resultados")
