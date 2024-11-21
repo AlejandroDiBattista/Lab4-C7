2e97ac6b85f466a73a272279aa99904342a39667
NachoConort <nachoconort@gmail.com>
2024-11-21 20:57:33 -0300

    Version final

--- practicos/05 - 59348 - Conort, Ignacio/tp8/ejercicio.py
+++ practicos/05 - 59348 - Conort, Ignacio/tp8/ejercicio.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 ## ATENCION: Debe colocar la direccion en la que ha sido publicada la aplicacion en la siguiente linea\
-# url = 'https://tp8-555555.streamlit.app/'
+# url = 'https://tp8-59348.streamlit.app/'
+# Hola profesor, tuve problemas para deployar debido a problemas con la dependencia maltpotlib y sus versiones. Podria ejecutar este codigo?
 
 def mostrar_informacion_alumno():
     with st.container(border=True):
@@ -15,81 +16,73 @@ def mostrar_informacion_alumno():
 
 mostrar_informacion_alumno()
 
-def cargar_datos(archivo):
+def cargarArchivo(archivo):
     try:
-        df = pd.read_csv(archivo)
-        return df
+        dataProductos = pd.read_csv(archivo)
+        return dataProductos
     except Exception as e:
         st.error(f"Error al cargar el archivo: {e}")
         return None
 
-def calcular_metricas(df):
-    df['Precio_promedio'] = df['Ingreso_total'] / df['Unidades_vendidas']
-    df['Margen_promedio'] = (df['Ingreso_total'] - df['Costo_total']) / df['Ingreso_total']
-
-    resumen = df.groupby('Producto').agg(
+def calcularValores(dataProductos):
+    dataProductos['Precio_promedio'] = (dataProductos['Ingreso_total'] / dataProductos['Unidades_vendidas'])
+    dataProductos['Margen_promedio'] = (dataProductos['Ingreso_total'] - dataProductos['Costo_total']) / dataProductos['Ingreso_total']
+    valoresProducto = dataProductos.groupby('Producto').agg(
         Unidades_vendidas=('Unidades_vendidas', 'sum'),
         Precio_promedio=('Precio_promedio', 'mean'),
         Margen_promedio=('Margen_promedio', 'mean')
     ).reset_index()
-    return resumen
+    return valoresProducto
 
-def graficar_evolucion(df, producto):
-    df_producto = df[df['Producto'] == producto]
+def graficarProducto(dataProductos, producto):
+    productoDP = dataProductos[dataProductos['Producto'] == producto]
+    productoDP['Fecha'] = pd.to_datetime(productoDP['Año'].astype(str) + '-' + productoDP['Mes'].astype(str) + '-01', format='%Y-%m-%d')
+    dataProductosSum = productoDP.groupby(['Fecha'])[['Unidades_vendidas']].sum().reset_index()
 
-    df_producto['Fecha'] = pd.to_datetime(df_producto['Año'].astype(str) + '-' + df_producto['Mes'].astype(str) + '-01', format='%Y-%m-%d')
-
-    df_sum = df_producto.groupby(['Fecha'])[['Unidades_vendidas']].sum().reset_index()
-
-    fig, ax = plt.subplots(figsize=(10, 6))
-
-    ax.plot(df_sum['Fecha'], df_sum['Unidades_vendidas'], label=producto)
-
-    z = np.polyfit(mdates.date2num(df_sum['Fecha']), df_sum['Unidades_vendidas'], 1)
+    grafico, eje = plt.subplots(figsize=(10, 6))
+    eje.plot(dataProductosSum['Fecha'], dataProductosSum['Unidades_vendidas'], label=producto)
+    z = np.polyfit(mdates.date2num(dataProductosSum['Fecha']), dataProductosSum['Unidades_vendidas'], 1)
     p = np.poly1d(z)
-    ax.plot(df_sum['Fecha'], p(mdates.date2num(df_sum['Fecha'])), linestyle='--', color='red')
-
-    ax.set_title(f"Evolución de Ventas Mensual")
-    ax.set_xlabel("Año-Mes")
-    ax.set_ylabel("Unidades Vendidas")
-    ax.tick_params(axis='x', rotation=45)
-    ax.legend(title="Producto")
+    eje.plot(dataProductosSum['Fecha'], p(mdates.date2num(dataProductosSum['Fecha'])), linestyle='--', color='red', label='Tendencia')
+    eje.set_title(f"Evolución de Ventas Mensual")
+    eje.set_xlabel("Año-Mes")
+    eje.set_ylabel("Unidades Vendidas")
+    eje.tick_params(axis='x', rotation=45)
+    eje.legend(title="Producto")
     plt.tight_layout()
 
-    return fig
+    return grafico
 
 def app():
-    st.title("Análisis de Ventas")
-
     st.sidebar.header("Carga archivo de datos")
     st.sidebar.write("Subir archivo CSV")
 
     archivo = st.sidebar.file_uploader("Sube tu archivo CSV", type="csv", label_visibility="collapsed", accept_multiple_files=False, key="archivo")
     
     if archivo is not None:
-        df = cargar_datos(archivo)
-        
-        if df is not None:
-            sucursal_seleccionada = st.sidebar.selectbox('Seleccionar Sucursal', ['Todas'] + df['Sucursal'].unique().tolist())
+        dataProductos = cargarArchivo(archivo)
+        if dataProductos is not None:
+            sucursalSeleccionada = st.sidebar.selectbox('Seleccionar Sucursal', ['Todas'] + dataProductos['Sucursal'].unique().tolist())
+            if sucursalSeleccionada == 'Todas':
+                st.title("Datos de Todas las Sucursales")
+            else:
+                st.title(f"Datos de {sucursalSeleccionada}")
+            
+            if sucursalSeleccionada != 'Todas':
+                dataProductos = dataProductos[dataProductos['Sucursal'] == sucursalSeleccionada]
 
-            if sucursal_seleccionada != 'Todas':
-                df = df[df['Sucursal'] == sucursal_seleccionada]
+            valoresProducto = calcularValores(dataProductos)
 
-            resumen = calcular_metricas(df)
-
-            for _, row in resumen.iterrows():
+            for _, row in valoresProducto.iterrows():
                 producto = row['Producto']
                 precio_promedio = row['Precio_promedio']
                 margen_promedio = row['Margen_promedio']
                 unidades_vendidas = row['Unidades_vendidas']
-
                 st.subheader(f"{producto}")
                 st.write(f"**Precio Promedio:** ${precio_promedio:.2f}")
                 st.write(f"**Margen Promedio:** {margen_promedio * 100:.2f}%")
                 st.write(f"**Unidades Vendidas:** {unidades_vendidas}")
-
-                st.pyplot(graficar_evolucion(df, producto))
-
+                st.pyplot(graficarProducto(dataProductos, producto))
         else:
             st.error("No se pudo cargar el archivo correctamente.")
     else:
