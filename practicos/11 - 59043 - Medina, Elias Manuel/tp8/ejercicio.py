e1500a016cf5596cfc48cc52bdf792f58bc3c36c
m4nu98 <160040306+m4nu98@users.noreply.github.com>
2024-11-21 17:48:20 -0300

    link

--- practicos/11 - 59043 - Medina, Elias Manuel/tp8/ejercicio.py
+++ practicos/11 - 59043 - Medina, Elias Manuel/tp8/ejercicio.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 ## ATENCION: Debe colocar la direccion en la que ha sido publicada la aplicacion en la siguiente linea\
-# url = 'https://tp8-555555.streamlit.app/'
+# url = 'https://tp8-parcial-medina-elias-manuel.streamlit.app/'
 
 def mostrar_informacion_alumno():
     with st.container(border=True):
